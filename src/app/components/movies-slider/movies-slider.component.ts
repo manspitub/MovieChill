e61b25e71c96e393ce9a3d89dcccfc17eaf61d2b
manspitub <manspitub@hotmail.com>
2026-01-17 11:16:14 +0100

    Enhance movie slider functionality with drag support and animation adjustments

--- src/app/components/movies-slider/movies-slider.component.ts
+++ src/app/components/movies-slider/movies-slider.component.ts
@@ -13,10 +13,16 @@ export class MoviesSliderComponent implements OnInit {
   private Movies = inject(MovieService);
 
   movies = signal<Movie[]>([]);
-
   paused = false;
-
   backdropPath: string = 'https://image.tmdb.org/t/p/original';
+
+  // Dragging related properties
+  isDragging = false;
+  startX = 0;
+  currentTranslate = 0;
+  prevTranslate = 0;
+
+  
 
   ngOnInit(): void {
     this.Movies.sliderMovies().subscribe((result) => {
@@ -31,5 +37,35 @@ export class MoviesSliderComponent implements OnInit {
 
   resumeCarousel() {
     this.paused = false;
+  }
+
+  onDragStart(event: MouseEvent | TouchEvent) {
+    this.pauseCarousel();
+    this.isDragging = true;
+
+    this.startX =
+      event instanceof TouchEvent ? event.touches[0].clientX : event.clientX;
+  }
+
+  onDragMove(event: MouseEvent | TouchEvent) {
+    if (!this.isDragging) return;
+
+    const currentX =
+      event instanceof TouchEvent ? event.touches[0].clientX : event.clientX;
+    this.currentTranslate = this.prevTranslate + currentX - this.startX;
+    
+  }
+
+  onDragEnd() {
+    this.isDragging = false;
+    this.prevTranslate = this.currentTranslate;
+    this.resumeCarousel();
+  }
+
+  getSliderStyle() {
+    return {
+      transform: `translateX(${this.currentTranslate}px)`,
+      transition: this.isDragging ? 'none' : 'transform 0.3s ease-out',
+    };
   }
 }
